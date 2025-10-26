f8c943af792214a48c558c5814b51e71dcb59210
Valdis Saulespurens <valdis.s.coding@gmail.com>
2025-10-26 21:17:23 +0200

    added simple hello py

---
+++ src/week1/hello.py
@@ -0,0 +1,6 @@
+# simple test to see if Python is working
+# get current year
+from datetime import datetime
+
+current_year = datetime.now().year
+print(f"Hello, RTU Programming Languages in Python Fall {current_year}!")
